aa14925d248448406375f6cf463a96df71d06023
zemiret <antos.mleczko@gmail.com>
2019-09-20 13:01:45 +0200

    Just registered models in admin.

--- dfys/core/admin.py
+++ dfys/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from dfys.core.models import Category, Activity, Skill, Comment, Attachment
+
+admin.site.register(Category)
+admin.site.register(Skill)
+admin.site.register(Activity)
+admin.site.register(Comment)
+admin.site.register(Attachment)
