53d8eb0b4c5b87ae5982df8ecbbc2666229502a0
zemiret <antos.mleczko@gmail.com>
2019-10-27 16:58:26 +0100

    Added base abstract model for tracking create/update timestamps.

--- dfys/core/models.py
+++ dfys/core/models.py
@@ -2,31 +2,39 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
+class TrackCreateModel(models.Model):
+    class Meta:
+        abstract = True
+
+    add_date = models.DateTimeField(auto_now_add=True)
+
+
+class TrackCreateUpdateModel(TrackCreateModel):
+    class Meta:
+        abstract = True
+
+    modify_date = models.DateTimeField(auto_now=True)
+
+
 class Category(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=128, blank=False)
     is_base_category = models.BooleanField(default=False)
 
 
-class Skill(models.Model):
+class Skill(TrackCreateModel):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
     name = models.CharField(max_length=128)
-    add_date = models.DateTimeField(auto_now_add=True)
 
 
-class Activity(models.Model):
+class Activity(TrackCreateUpdateModel):
     title = models.CharField(max_length=128)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     description = models.TextField(blank=True, default='')
-    add_date = models.DateTimeField(auto_now_add=True)
-    modify_date = models.DateTimeField(auto_now=True)
 
 
-class ActivityEntry(models.Model):
+class ActivityEntry(TrackCreateUpdateModel):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    add_date = models.DateTimeField(auto_now_add=True)
-    modify_date = models.DateTimeField(auto_now=True)
     comment = models.TextField(blank=True)
